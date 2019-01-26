5db2bc829892fb053dda15eedcc7090ffe951b38
Miguel Company <m.company.company@gmail.com>
2019-01-26 04:36:42 +0100

    Definición de intersecciones y rejilla

---
+++ UnityProject/Assets/Code/DataModels/IntersectionGrid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class IntersectionGrid : ScriptableObject
+{
+    [System.Serializable]
+    struct IntersectionDefinition
+    {
+        int index;
+        IntersectionModel model;
+    }
+
+    [SerializeField]
+    IntersectionModel defaultModel;
+
+    [SerializeField]
+    IntersectionDefinition[] intersections;
+}
---
+++ UnityProject/Assets/Code/DataModels/IntersectionModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class IntersectionModel : ScriptableObject
+{
+    [SerializeField]
+    float [] speedup;
+
+    public float GetRandomSpeedup()
+    {
+        return speedup[UnityEngine.Random.Range(0, speedup.Length)];
+    }
+}
